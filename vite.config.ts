433e299bc4b7bc021cfde4875d559cdfc9a40a45
Testicode <osin7179@gmail.com>
2026-05-05 16:52:47 +0100

    fix: removed vite rolldown for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+// import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+// import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] }),
+    // babel({ presets: [reactCompilerPreset()] }),
   ],
   resolve: {
     alias: {
